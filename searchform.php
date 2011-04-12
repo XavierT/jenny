ccec040ba330e7434278e461411c7d063783cb03
Mycroft <sseingalt@gmail.com>
2011-04-12 21:10:05 +0200

    translate search text

--- searchform.php
+++ searchform.php
@@ -1,3 +1,3 @@
 <form method="get" id="searchform" action="<?php home_url(); ?>/">
-	<div><input type="text" value="Type some keywords and press Enter." name="s" id="s" onfocus="if (this.value == 'Type some keywords and press Enter.') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Type some keywords and press Enter.';}" /></div>
+	<div><input type="text" value="Entrez un mot clé et tapez sur Entrée" name="s" id="s" onfocus="if (this.value == 'Entrez un mot clé et tapez sur Entrée') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Entrez un mot clé et tapez sur Entrée';}" /></div>
 </form>
